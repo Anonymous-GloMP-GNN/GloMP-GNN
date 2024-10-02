d0bf9dcd83f2ad3da813cbbd0a37fb6b07e86b12
Anonymous-GloMP-GNN <147825155+Anonymous-GloMP-GNN@users.noreply.github.com>
2024-10-02 20:05:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -24,17 +24,17 @@ NORMALIZATION = {
 }
 
 class MyModel(nn.Module):
-    def __init__(self, args, input_dim, hidden_dim,output_dim,hidden_dim_multiplier, num_heads,num_nodes,normalization, dropout,number_of_edges, num_layers):
+    def __init__(self, args, input_dim, hidden_dim,output_dim,hidden_dim_multiplier, num_heads,num_nodes,xx_initial, normalization, dropout,number_of_edges, num_layers):
         super(MyModel, self).__init__()
         self.args = args
-
+        self.xx = xx_initial
         normalization = NORMALIZATION[normalization]
-        # self.filter = MaxwellDemonFilter(hidden_dim, hidden_dim,  hidden_dim, hidden_dim_multiplier, num_heads,dropout,number_of_edges)
         self.input_linear = nn.Linear(in_features=input_dim, out_features=hidden_dim)
         self.residual_modules = nn.ModuleList()
         for _ in range(num_layers):
-            residual_module = ResidualModuleWrapper(module=MaxwellDemonFilter,
+            residual_module = ResidualModuleWrapper2(module=MaxwellDemonFilter,
                                                     normalization=normalization,
+                                                    xx_initial = xx_initial,
                                                     dim=hidden_dim,
                                                     hidden_dim_multiplier=hidden_dim_multiplier,
                                                     num_heads=num_heads,
@@ -51,7 +51,6 @@ class MyModel(nn.Module):
         self.output_normalization = normalization(hidden_dim * (num_layers+1))
         self.output_linear = nn.Linear(in_features=hidden_dim * (num_layers+1), out_features=output_dim)
 
-
     def forward(self,graph,x):
         x = self.input_linear(x)
         x = self.dropout(x)
@@ -61,9 +60,8 @@ class MyModel(nn.Module):
             x = residual_module(graph, x)
             x_all = torch.cat([x_all,x], dim = -1)
 
-        x_all = self.output_normalization(x_all)
-        x_all = self.output_linear(x_all).squeeze(1)
+        x_all_0 = self.output_normalization(x_all)
+        x_all = self.output_linear(x_all_0).squeeze(1)
 
-        return x_all
-
+        return x_all,x_all_0
 
